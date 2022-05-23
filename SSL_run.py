d0683339dccb8cd59961ab7ed255c3a134b4bff5
gaurav <gaurav.chauhan1@rksv.in>
2022-05-23 09:11:11 +0530

    vfv

--- SSL_run.py
+++ SSL_run.py
@@ -12,21 +12,30 @@ import stat
 import subprocess
 import sys
 
-STAT_0o775 = ( stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR
-             | stat.S_IRGRP | stat.S_IWGRP | stat.S_IXGRP
-             | stat.S_IROTH |                stat.S_IXOTH )
+STAT_0o775 = (
+    stat.S_IRUSR
+    | stat.S_IWUSR
+    | stat.S_IXUSR
+    | stat.S_IRGRP
+    | stat.S_IWGRP
+    | stat.S_IXGRP
+    | stat.S_IROTH
+    | stat.S_IXOTH
+)
 
 
 def main():
     openssl_dir, openssl_cafile = os.path.split(
-        ssl.get_default_verify_paths().openssl_cafile)
+        ssl.get_default_verify_paths().openssl_cafile
+    )
 
     # +++> if already done  <----
-    #print(" -- pip install --upgrade certifi")
-    #subprocess.check_call([sys.executable,
+    # print(" -- pip install --upgrade certifi")
+    # subprocess.check_call([sys.executable,
     #    "-E", "-s", "-m", "pip", "install", "--upgrade", "certifi"])
 
     import certifi
+
     # change working directory to the default SSL directory
     os.chdir(openssl_dir)
     relpath_to_certifi_cafile = os.path.relpath(certifi.where())
@@ -41,5 +50,6 @@ def main():
     os.chmod(openssl_cafile, STAT_0o775)
     print(" -- update complete")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
